005de289e939b17f6d8bf20e55e874a5e4b49616
Ashutosh Tiwari <tiwariashutosh12817@gmail.com>
2025-03-23 21:00:19 +0530

    commit

--- api.py
+++ api.py
@@ -64,3 +64,6 @@ def get_audio(company_name: str):
         raise HTTPException(status_code=500, detail=str(e))
     
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=7860)
