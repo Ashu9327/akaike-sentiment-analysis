c742267d2bdddf3f2a4540efb8d694a58c491345
Ashutosh Tiwari <tiwariashutosh12817@gmail.com>
2025-03-23 20:52:43 +0530

    commit

--- app.py
+++ app.py
@@ -7,10 +7,9 @@ import pyperclip
 import os
 
 # Check if running on Hugging Face
-if "HF_SPACE" in os.environ:
-    BASE_URL = "https://huggingface.co/spaces/mlai23/Companies_Sentiment_Analysis"  # Replace with your actual URL
-else:
-    BASE_URL = "http://127.0.0.1:8001/api"
+
+BASE_URL = "https://huggingface.co/spaces/mlai23/Companies_Sentiment_Analysis"  # Replace with your actual URL
+
 
 
 # Companies List
